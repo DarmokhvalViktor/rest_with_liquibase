d248a86a312b5d596529cac249623c354e20a195
Darmokhval Viktor <darmokhvalviktor@gmail.com>
2024-04-19 13:11:01 +0300

    Initial commit

---
+++ src/main/java/com/darmokhval/rest_with_liquibase/RestWithLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.darmokhval.rest_with_liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithLiquibaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darmokhval/rest_with_liquibase/RestWithLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darmokhval.rest_with_liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
